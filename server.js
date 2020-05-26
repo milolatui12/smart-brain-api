e5f14440211abeee501d39a3b23934f2cdcd6a80
milo <tranmilo66@gmail.com>
2020-05-26 08:59:39 +0700

    add file

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+var knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : '123456',
+        database : 'smart_brain'
+    }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if(isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                name: name, 
+                email: loginEmail[0],
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0])
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })       
+    .catch(err => res.status(400).json('unable to signup'));
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+     .then(user => {
+         if(user.length) {
+             res.json(user);
+         } else {
+             res.status(400).json('not found');
+         }
+     });
+}) 
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries)
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(3001, () => {
+    console.log('app is running')
+})
